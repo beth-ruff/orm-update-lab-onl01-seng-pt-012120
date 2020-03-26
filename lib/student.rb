99ca667ba0628d2943bbc44c862c94f0edf6dd87
Beth Ruff <beth.ruff2@gmail.com>
2020-03-26 02:11:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,10 +48,10 @@ attr_reader :id
  end 
  
  def self.new_from_db(row)
-   new_student = self.new 
+   new_student = Student.new 
    new_student.id = row[0]
    new_student.name = row[1]
-   new_student.length = row[2]
+   new_student.grade = row[2]
    new_student
  end 
 
