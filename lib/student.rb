2f3a86338a1c98a3913e3548ba786795df09919c
Beth Ruff <beth.ruff2@gmail.com>
2020-03-26 02:20:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,8 +47,11 @@ attr_accessor :id, :name, :grade
  end 
  
  def self.new_from_db(row)
-    student_new = self.new(row[0], row[1], row[2])
-    student_new
+   new_student = Student.new(id, name, grade)
+   new_student.id = row[0]
+   new_student.name = row[1]
+   new_student.grade = row[2]
+   new_student
  end 
  
  def self.find_by_name(name)
